7fcb11ec49e4780dd29d036aeb87139edb973b2f
Dibyajyoti Mondal <54498482+djm-1@users.noreply.github.com>
2020-11-27 11:57:27 +0530

    Solution of 5%_mean

---
+++ 1. Array/5%_mean/5%_mean.cpp
@@ -0,0 +1,50 @@
+/**
+ * 5%_mean.cpp
+ * Solution of 5% mean problem
+ * First sort the elements in ascending order.
+ * Then calculate value of 5% of n (rounded of to an integer)
+ * Let x is the required value.
+ * So we have to find the average of elements excluding first and last x elements
+ * Excluding elements from 0 to x-1 index and n-x to n-1 index
+ * then run a for loop for finding the sum and divide it by n-2x for getting the average
+ * djm_boss [Dibyajyoti Mondal](https://github.com/djm-1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Solve function
+void solve() {
+
+  //taking input
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int i=0;i<n;i++)
+  {
+      cin>>arr[i];
+  }
+
+  //sorting the array in ascending order
+  sort(arr.begin(),arr.end());
+  int x=n/20;   // 5% of n= (n*5)/100 = n/20
+  int sum=0;
+  for(int i=x;i<n-x;i++)
+  {
+      sum+=arr[i];
+  }
+  float avg=(float)sum/(n-2*x);
+  printf("%.5f\n",avg);
+}
+
+
+
+// Main function
+int main() {
+  int t;
+  cin>>t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
